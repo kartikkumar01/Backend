ddef3a008800eaf5a6486ea357828338f52877f1
Kartik Kumar <kartikjhaba@gmail.com>
2025-08-08 21:43:44 +0530

    php till oops

---
+++ 00 Basic PHP with OOPS/OOPS/01.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+Procedural Programming(C) is a programming style where focus is on logic and functions.
+1) Focuses on functions and logic.
+2) The program is a sequence of instructions and procedure calls.
+3) Data and functions are separate. (Khilra hua code)
+4) Harder to manage in large codebases, since everything is global or passed around.
+*/
+
+/*
+OOP is a programming style where you model real-world entities as objects.
+1) Focuses on objects, which represent real-world entities.
+2) Code is organized or grouped into classes with properties (data) and methods (functions).
+3) Promotes encapsulation, reusability, and scalability.
+4) Easier to maintain and extend large applications.
+*/
+?>
